694e69c3339fa0d03b07431f4c847f23bd3d5b1d
kevin <kanzelm4@gmail.com>
2016-03-28 19:35:46 -0500

    removed bad unit test

--- src/test/java/com/github/kkanzelmeyer/alfred/datamodel/DatamodelTest.java
+++ src/test/java/com/github/kkanzelmeyer/alfred/datamodel/DatamodelTest.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import com.github.kkanzelmeyer.alfred.datamodel.StateDevice.Builder;
 import com.github.kkanzelmeyer.alfred.datamodel.enums.State;
 import com.github.kkanzelmeyer.alfred.datamodel.enums.Type;
-import com.github.kkanzelmeyer.alfred.plugins.WebcamMotionPlugin;
 
 public class DatamodelTest
 {
@@ -37,38 +36,6 @@ public class DatamodelTest
     assertEquals("Device management test - devices should be equal", doorbell, copy);
     
     // clean up the state
-    StateDeviceManager.INSTANCE.removeStateDevice(doorbell);
-  }
-
-  @Test
-  public void stateTest()
-  {
-    // create new device and add it to the data model
-    LOG.debug("State Test");
-    LOG.debug("Creating state device");
-    StateDevice doorbell = new StateDevice(new Builder()
-        .setId("front-door")
-        .setName("Front Door")
-        .setState(State.INACTIVE)
-        .setType(Type.DOORBELL_WEBCAM)
-        .build());
-
-    LOG.debug("Adding device to device manager");
-    StateDeviceManager.INSTANCE.addStateDevice(doorbell);
-
-    LOG.debug("Activating new plugin");
-    WebcamMotionPlugin plugin = new WebcamMotionPlugin(doorbell);
-    plugin.activate();
-
-    // set a new state
-    LOG.debug("Injecting new device state");
-    StateDeviceManager.INSTANCE.updateStateDevice(doorbell.getId(), State.ACTIVE);
-
-    assertEquals("State change test - states should be equal",
-        StateDeviceManager.INSTANCE.getDevice(doorbell.getId()).getState(), State.ACTIVE);
-
-    // clean up the state
-    plugin.deactivate();
     StateDeviceManager.INSTANCE.removeStateDevice(doorbell);
   }
 
