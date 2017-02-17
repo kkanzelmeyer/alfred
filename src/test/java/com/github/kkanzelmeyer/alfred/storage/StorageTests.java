c6366a807b8a5bb7f0479785e3aa0357ffa932be
Kevin Kanzelmeyer <kanzelm4@gmail.com>
2017-02-16 23:04:59 -0600

    working on understanding gradle tests and build

--- src/test/java/com/github/kkanzelmeyer/alfred/storage/StorageTests.java
+++ src/test/java/com/github/kkanzelmeyer/alfred/storage/StorageTests.java
@@ -1,14 +1,13 @@
 package com.github.kkanzelmeyer.alfred.storage;
 
-import com.github.kkanzelmeyer.alfred.datastore.Store;
-import com.github.kkanzelmeyer.alfred.server.Config;
+import java.io.File;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
+import com.github.kkanzelmeyer.alfred.datastore.Store;
+import com.github.kkanzelmeyer.alfred.server.Config;
 
 /**
  * Created by kevinkanzelmeyer on 2/16/17.
@@ -22,18 +21,20 @@ public class StorageTests {
       // load the api key
       ClassLoader classLoader = getClass().getClassLoader();
       File file = new File(classLoader.getResource("config.json").getFile());
+      logger.info("config file: {}", file.getAbsolutePath());
       Config config = Config.createConfig(file.getAbsolutePath());
       logger.info("auth path: {}", config.authFilePath);
       Store.INSTANCE.setConfig(config);
 
       // set up storage service
-      for (String service : Store.INSTANCE.getConfig().storageServices) {
-        StorageBridge.INSTANCE.addService(service);
-      }
-      StorageBridge.INSTANCE.setup();
-
-      // get test image
-      BufferedImage originalImage = ImageIO.read(new File("src/test/resources/kanzelmeyer-software-company.jpg"));
+      // for (String service : Store.INSTANCE.getConfig().storageServices) {
+      // StorageBridge.INSTANCE.addService(service);
+      // }
+      // StorageBridge.INSTANCE.setup();
+      //
+      // // get test image
+      // BufferedImage originalImage = ImageIO.read(new
+      // File("src/test/resources/kanzelmeyer-software-company.jpg"));
 //      StorageBridge.INSTANCE.saveImage(originalImage);
     } catch (Exception e) {
       e.printStackTrace();
